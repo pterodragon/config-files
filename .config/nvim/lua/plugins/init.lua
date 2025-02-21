43de3559272495685152c56211f21a906f86583b
wing <poonlokwing@gmail.com>
2025-02-21 20:56:35 +0000

    treesitter text object config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -4,8 +4,6 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -14,12 +12,10 @@ return {
   },
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
+      require("nvim-surround").setup {}
     end,
   },
   {
@@ -36,7 +32,7 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gj", -- set to `false` to disable one of the mappings
+          init_selection = "gj",
           node_incremental = "gj",
           scope_incremental = "gl",
           node_decremental = "gk",
@@ -65,79 +61,62 @@ return {
           select = {
             enable = true,
 
-            -- Automatically jump forward to textobj, similar to targets.vim
             lookahead = true,
 
             keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
               ["af"] = "@function.outer",
               ["if"] = "@function.inner",
               ["ac"] = "@class.outer",
-              -- You can optionally set descriptions to the mappings (used in the desc parameter of
-              -- nvim_buf_set_keymap) which plugins like which-key display
-              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-              -- You can also use captures from other query groups like `locals.scm`
-              ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+              ["ic"] = "@class.inner",
+              ["id"] = "@conditional.inner",
+              ["ad"] = "@conditional.outer",
+              ["im"] = "@comment.inner",
+              ["am"] = "@comment.outer",
+              ["ie"] = "@return.inner",
+              ["ae"] = "@return.outer",
+              ["io"] = "@loop.inner",
+              ["ao"] = "@loop.outer",
+              ["al"] = "@assignment.lhs",
+              ["ar"] = "@assignment.rhs",
+              ["at"] = "@statement.outer",
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
             selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "<c-v>", -- blockwise
+              ["@parameter.outer"] = "v",
+              ["@function.outer"] = "V",
+              ["@class.outer"] = "V",
+              ["@conditional.inner"] = "V",
+              ["@conditional.outer"] = "V",
+              ["@return.inner"] = "V",
+              ["@return.outer"] = "V",
+              ["@loop.inner"] = "V",
+              ["@loop.outer"] = "V",
+              ["@comment.inner"] = "V",
+              ["@comment.outer"] = "V",
+              ["@statement.outer"] = "V",
+              ["@assignment.lhs"] = "v",
+              ["@assignment.rhs"] = "v",
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true or false
             include_surrounding_whitespace = false,
           },
           move = {
             enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
+            set_jumps = true,
             goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = { query = "@class.outer", desc = "Next class start" },
-              --
-              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+              ["]f"] = "@function.outer",
+              ["]c"] = "@class.outer",
               ["]o"] = "@loop.*",
-              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-              --
-              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-              ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
-              ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
             },
             goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
+              ["]F"] = "@function.outer",
+              ["]C"] = "@class.outer",
             },
             goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
+              ["[f"] = "@function.outer",
+              ["[c"] = "@class.outer",
             },
             goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-            -- Below will go to either the start or the end, whichever is closer.
-            -- Use if you want more granular movements
-            -- Make it even more gradual by adding multiple queries and regex.
-            goto_next = {
-              ["]d"] = "@conditional.outer",
-            },
-            goto_previous = {
-              ["[d"] = "@conditional.outer",
+              ["[F"] = "@function.outer",
+              ["[C"] = "@class.outer",
             },
           },
         },
